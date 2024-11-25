3fd25230d2cb1395dc5df3eacef35c4176a5fd03
Quardin Lyttle <lyttleqd@gmail.com>
2024-11-25 09:16:30 -0500

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,15 +1,19 @@
 `include "mux.v"
-module ALU(operandA,operandB,ImEx,opcode,func3,func7);
+module ALU(operandA,operandB,ImEx,opcode,func3,func7,result);
 
 input [31:0] operandA, operandB,ImEx;
-input [6:0] opcode
+input [6:0] opcode, func7;
+input [2:0] func3;
+output [31:0] result;
+wire [31:0] sourceB;
 
 mux #(32)SrcB(
-        .muxOut(operandB),
-        .dataA(op),
-        .dataB(dataB),
-        .select(select)
+        .muxOut(sourceB),
+        .dataA(operandB),
+        .dataB(ImEx),
+        .select(opcode[5])
     );
+
 
 
 endmodule
